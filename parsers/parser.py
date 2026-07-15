48671a67532c7f05cccb42e10225e667544fedbb
pulkitchopra3007 <yopulkit13@gmail.com>
2026-07-15 21:17:47 +0530

    Update parser.py

--- parsers/parser.py
+++ parsers/parser.py
@@ -1,11 +1,20 @@
 """
-Base Parser
-
-Every parser in this project must inherit from this class.
+Resume Parser
 """
 
+import re
 
-class DocumentParser:
 
-    def parse(self, file_path):
-        raise NotImplementedError("Parser not implemented.")
+class ResumeParser:
+
+    def __init__(self, text):
+        self.text = text
+
+    def get_email(self):
+        pattern = r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}"
+        match = re.search(pattern, self.text)
+
+        if match:
+            return match.group()
+
+        return None
