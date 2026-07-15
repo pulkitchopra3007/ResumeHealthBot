a3866b1de9df811f84b431e5ea122ca71e2f46b5
pulkitchopra3007 <yopulkit13@gmail.com>
2026-07-15 20:40:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,19 @@ Main Entry Point
 from telegram.ext import (
     Application,
     CommandHandler,
+    MessageHandler,
+    filters,
 )
 
 from config import BOT_NAME, BOT_VERSION, BOT_TOKEN
-from bot.handlers import start
+from bot.handlers import start, receive_resume
 
 
 def main():
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.Document.PDF, receive_resume))
 
     print(f"{BOT_NAME} v{BOT_VERSION} Started Successfully")
 
