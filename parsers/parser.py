f00a83be20e20df74fb0927a3a1673bbb46a3856
pulkitchopra3007 <yopulkit13@gmail.com>
2026-07-15 19:54:48 +0530

    Update parser.py

--- parsers/parser.py
+++ parsers/parser.py
@@ -1,1 +1,11 @@
-kyu bc
+"""
+Base Parser
+
+Every parser in this project must inherit from this class.
+"""
+
+
+class DocumentParser:
+
+    def parse(self, file_path):
+        raise NotImplementedError("Parser not implemented.")
