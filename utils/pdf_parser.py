d23ef739e2da44ef61b24808855f2fb993047522
pulkitchopra3007 <yopulkit13@gmail.com>
2026-07-15 21:14:08 +0530

    Update pdf_parser.py

--- utils/pdf_parser.py
+++ utils/pdf_parser.py
@@ -1,13 +1,15 @@
-import fitz
+from pypdf import PdfReader
+
 
 def extract_text(pdf_path):
+    reader = PdfReader(pdf_path)
+
     text = ""
 
-    doc = fitz.open(pdf_path)
+    for page in reader.pages:
+        page_text = page.extract_text()
 
-    for page in doc:
-        text += page.get_text()
-
-    doc.close()
+        if page_text:
+            text += page_text + "\n"
 
     return text
