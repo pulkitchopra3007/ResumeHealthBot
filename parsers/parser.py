a1e930e4aa78334dde5568dd41c17faad4393bb8
pulkitchopra3007 <yopulkit13@gmail.com>
2026-07-15 21:28:34 +0530

    Update parser.py

--- parsers/parser.py
+++ parsers/parser.py
@@ -1,20 +1,21 @@
-import re
+"""
+Resume Parser
+"""
+
+from parsers.extractors import (
+    EmailExtractor,
+    PhoneExtractor,
+)
 
 
 class ResumeParser:
+
     def __init__(self, text: str):
         self.text = text
 
-    def get_email(self):
-        match = re.search(
-            r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}",
-            self.text,
-        )
-        return match.group(0) if match else "Not Found"
+    def parse(self):
 
-    def get_phone(self):
-        match = re.search(
-            r"(\+91[\s-]?)?[6-9]\d{9}",
-            self.text,
-        )
-        return match.group(0) if match else "Not Found"
+        return {
+            "email": EmailExtractor.extract(self.text),
+            "phone": PhoneExtractor.extract(self.text),
+        }
